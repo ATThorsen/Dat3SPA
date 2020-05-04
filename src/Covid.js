e77a4a62aac8c1893936e9df792573cf36d26264
Malthorn1 <mikkelskott@gmail.com>
2020-05-04 13:07:24 +0200

    covid

--- src/Covid.js
+++ src/Covid.js
@@ -9,53 +9,56 @@ import facade from "./apiFacade";
 
 
 export default function Covid() {
-  const [active, setActive] = useState();
-  const [recovered, setRecovered] = useState();
-  const [deaths, setDeaths] = useState();
-  const [covidData, setCovidData] = useState();
+
+  const [covidData, setCovidData] = useState([]);
 
    function getCovidInfo() {
     let spot = document.getElementById("spotID").value; 
     let options = facade.makeOptions("GET", true)
     fetch("http://localhost:8080/dat3/api/all/info/"+ spot, options)
       .then((res) => res.json())
-      .then((data) => {
-          console.log(data)
-        if(data.code != "403") {
-           // setActive(data.CovidDTO[0].Active);
-           setCovidData(data)
-            console.log("hallo")
-             setRecovered(data.CovidDTO[0].Recovered);
-            // setDeaths(data.Deaths);
-            covidNumbers();
-             
-        }
-      });
-  }
+      .then((data) => setCovidData(data)
+     ); }
+    console.log(covidData.CovidDTO) ; 
+    
+   
+  //     //  if(data.code != "403") {
 
-  function covidNumbers() {
-    return active ? (
-      <>
-        <h1>Covid info:</h1>
-        <h4>Active cases: {active}</h4>
-        <h4>Recovered cases: {recovered}</h4>
-        <h4>Deaths : {deaths}</h4>
-      </>
-    ) : (
-      <></>
-    );
-  }
+  //    //      setCovidData(data)
+  //          console.log(covidData)
+  //           console.log("hallo")
+  //        //    setRecovered(data.CovidDTO[0].Recovered);
+
+  //        //   covidNumbers();
+             
+  //       }
+  //     };
+  // }
+
+  // function covidNumbers() {
+  //   return active ? (
+  //     <>
+  //       <h1>Covid info:</h1>
+  //       <h4>Active cases: {active}</h4>
+  //       <h4>Recovered cases: {recovered}</h4>
+  //       <h4>Deaths : {deaths}</h4>
+  //     </>
+  //   ) : (
+  //     <></>
+  //   );
+
 
   return (
     <div className="App">
       <br />
       <input type="text" id="spotID" name="spotName" placeholder="City or country"></input>
       <button onClick={getCovidInfo}>Get Covid Info</button>
-      {covidNumbers()}
+      {/* {covidNumbers()} */}
     </div>
   );
-}
 
+
+}
 
    function getCovidData (data) {
   
